8a5334844643e1365cf69e5a8c74625b40b41ac1
JinGoon-Kim <raneata@gmail.com>
2021-08-31 22:36:13 +0900

    added: video delete and search

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,8 +1,9 @@
-export const join = (req, res) => res.send("Join");
-export const see = (req, res) => {
-    console.log(req.params);
-    return res.send("Watch");
-}
+export const getJoin = (req, res) => res.render("join", {pageTitle: "Join"});
+export const postJoin = (req, res) => {
+    console.log(req.body);
+    res.end();
+};
+export const see = (req, res) => res.send("see");
 export const edit = (req, res) => res.send("Edit");
 export const remove = (req, res) => res.send("Remove User");
 export const login = (req, res) => res.send("Login");
--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,5 @@
-import Video from "../models/Video";
 import {raw} from "express";
+import Video from "../models/Video";
 
 /*
 export const home = async (req, res) => {
@@ -10,7 +10,7 @@ export const home = async (req, res) => {
 */
 export const home = async (req, res) => {
     try {
-        const videos = await Video.find({});
+        const videos = await Video.find({}).sort({createdAt:"desc"});
         return res.render("home", {pageTitle: "Home", videos })
     }catch(err) {
         return res.render("server-error", err);
@@ -40,8 +40,7 @@ export const postEdit = async (req, res) => {
     if (!video)
         return res.render("404", {pageTitle: "video not found."});
     await Video.findByIdAndUpdate(id, {
-        title, description, hashtags:hashtags
-            .split(",").map((word) => word.startsWith('#') ? word : `#${word}`)
+        title, description, hashtags: Video.formatHashtags(hashtags),
     })
     return res.redirect(`/videos/${id}`);
 };
@@ -66,7 +65,7 @@ export const postUpload = async (req, res) => {
         await Video.create({
             title,
             description,
-            hashtags: hashtags.split(",").map(word => `#${word}`),
+            hashtags: Video.formatHashtags(hashtags),
         });
         return res.redirect("/");
     }catch(err) {
@@ -76,3 +75,22 @@ export const postUpload = async (req, res) => {
         });
     }
 };
+
+export const deleteVideo = async (req, res) => {
+    const { id } = req.params;
+    await Video.findByIdAndDelete(id);
+    return res.redirect("/")
+;}
+
+export const search = async (req, res) => {
+    const {keyword} = req.query;
+    let videos = [];
+    if(keyword){
+        videos = await Video.find({
+            title: {
+                $regex: new RegExp(keyword, "i")
+            },
+        });
+    }
+    return res.render("search", {pageTitle: "Search", videos});
+}
