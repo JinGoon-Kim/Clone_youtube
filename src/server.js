417b0d225ae1d3b6d16d39c890cdc11ed075535f
JinGoon-Kim <raneata@gmail.com>
2021-09-07 22:01:50 +0900

    created: login and session

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@
 
 import express from "express";
 import morgan from "morgan";
+import session from "express-session";
 import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
@@ -13,6 +14,26 @@ app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use(express.urlencoded({extended: true}));
+
+app.use(
+    session({
+        secret: "Hello!",
+        resave: true,
+        saveUninitialized: true,
+    })
+);
+
+app.use((req, res, next) => {
+   req.sessionStore.all((error, sessions) => {
+       console.log(sessions);
+       next();
+   })
+});
+
+app.get("/add-one", (req, res, next) =>{
+    req.session.potato += 1;
+    return res.send(`${req.session.id}\n${req.session.potato}`);
+})
 
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
