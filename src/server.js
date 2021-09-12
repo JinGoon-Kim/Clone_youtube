6822ae8b7297f4f677fec5c4f0650f350a5940d1
JinGoon-Kim <raneata@gmail.com>
2021-09-12 22:17:51 +0900

    create: complete login

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ import session from "express-session";
 import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
+import {localsMiddleware} from "./middlewares";
 
 const app = express();
 const logger = morgan("dev");
@@ -22,13 +23,7 @@ app.use(
         saveUninitialized: true,
     })
 );
-
-app.use((req, res, next) => {
-   req.sessionStore.all((error, sessions) => {
-       console.log(sessions);
-       next();
-   })
-});
+app.use(localsMiddleware);
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
